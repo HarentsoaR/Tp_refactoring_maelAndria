48a0ec0d24e86878ba75ee0adb628240aa6f188c
HarentsoaR <randriamaholimanana1@gmail.com>
2024-04-03 19:14:59 +0300

    Coder class Homme

--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -1,5 +1,43 @@
 package iut.bad;
 
 public class Homme {
+    private String nom;
+    private String prenom;
+    private int age;
 
+    // Constructeur par défaut
+    public Homme() {
+    }
+
+    // Constructeur paramétré
+    public Homme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    // Getters et Setters pour chaque champ
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
